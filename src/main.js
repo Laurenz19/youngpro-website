31b61231bc5720b71977d91a97097d9d5dc6e7d9
Laurenz19 <laurenziosambany@gmail.com>
2022-02-18 00:38:33 +0300

    create the basic shape of the website

--- src/main.js
+++ src/main.js
@@ -2,5 +2,21 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'material-icons/iconfont/material-icons.css';
 
-createApp(App).use(store).use(router).mount('#app')
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas);
+
+import { fab } from '@fortawesome/free-brands-svg-icons';
+library.add(fab);
+
+import { dom } from "@fortawesome/fontawesome-svg-core";
+dom.watch();
+
+/* add font awesome icon component */
+createApp(App)
+    .component("fa", FontAwesomeIcon)
+    .use(store).use(router).mount('#app')
---
+++ src/views/front-office/Index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="index">
+   <header>
+     <div id="title">Young Professional</div>
+     <nav id="app-nav">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Our team</li>
+        <li>Forum</li>
+        <li>Contact</li>
+      </ul>
+     </nav>
+   </header>
+   <main>
+     <div id="young-pro">
+       young pro introduction
+     </div>
+     <div id="content">
+       my content
+     </div>
+
+   </main>
+   <footer>
+    <div id="copyright-info">
+      Designed and developed by <span> Young professional Team </span>  | © 2022
+    </div>
+    <ul>
+      <li> <fa :icon="['fab', 'facebook']" size="lg"/> </li>
+      <li> <fa :icon="['fab', 'linkedin']" size="lg"/> </li>
+      <li> <fa :icon="['fab', 'github']" size="lg"/> </li>
+    </ul>
+   </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+  @import url('../../assets/css/front.css');
+</style>
